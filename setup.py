ad46f5bfe3750ffabbc684b7bf05164286f60d82
Park Sung Min <vamf12@gmail.com>
2016-01-05 14:22:57 +0900

    set version 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
         name='sqlalchemy-paginate',
-        version='0.0.1',
+        version='0.0.2',
         packages=['sqlalchemy_paginate'],
         zip_safe=False,
         install_requires=['sqlalchemy>=1.0.11']
